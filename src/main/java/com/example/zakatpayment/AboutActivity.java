6a2a9904cf8fe7e41b6af89f13c3e261b4334948
Auni13 <120319888+Auni13@users.noreply.github.com>
2022-12-17 02:26:52 +0800

    Update AboutActivity.java

--- src/main/java/com/example/zakatpayment/AboutActivity.java
+++ src/main/java/com/example/zakatpayment/AboutActivity.java
@@ -1,6 +1,8 @@
 package com.example.zakatpayment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView
 
 import android.os.Bundle;
 
@@ -10,5 +12,11 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        setupHyperlink();
+    }
+    
+    private void setupHyperlink(){
+        TextView linkTextView = findViewById(R.id);
+        linkTextView.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
